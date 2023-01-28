dce0659f186be63a484f57fc9a906726aee3557f
anupama.g <anupama.g@AcusisBlr.lan>
2023-01-28 21:34:23 +0530

    demo on Area

---
+++ Area.java
@@ -0,0 +1,18 @@
+
+public class Area {
+
+	public Area() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float radius=6.5f;
+final float pi=3.14f;
+double area= pi*radius*radius;
+
+System.out.println("Area of circle is " + area);
+
+	}
+
+}
