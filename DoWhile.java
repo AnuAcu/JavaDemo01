3966a9a6fc507e9ae2dc3d8a4e4750c506d61e9b
anupama.g <anupamagiris@gmail.com>
2023-01-28 23:01:48 +0530

    Added do while program

---
+++ DoWhile.java
@@ -0,0 +1,24 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//when condition of expression is false then body of loop is not executed
+		//it is required to execute body of loop atleast once even if condition is false
+		/*
+		do
+		{
+			<<>statement>>
+		}while condition
+		*/
+		
+		int num=3;
+		do
+		{
+			System.out.println("welcome to dowhile" );
+			num--;
+		}while (num>3);
+	}
+
+}
