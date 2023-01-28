db593520bebec0213309db44cd07c690e474be78
anupama.g <anupama.g@AcusisBlr.lan>
2023-01-28 21:54:03 +0530

    Adding 3rd file

---
+++ Reg.java
@@ -0,0 +1,40 @@
+
+public class Reg {
+
+	public Reg() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 
+		 Student name
+		 ph no
+		 branch
+		 email
+		 college
+		 passout
+		 backlogs
+		 */
+  String name="Anupama";
+  String email="anupamagiris@gmail.com";
+  String branch ="Textile";
+  long phone=9632541680L;
+  short passout=2004;
+  boolean backlogs=false;
+  
+  System.out.println("Student name "  +  name);
+  System.out.println("Student email "  +  email);
+  System.out.println("Student Phone Number "  +  phone);
+  System.out.println("Student year of passout "  +  passout);
+  System.out.println("Student backlogs "  +  backlogs);
+  
+  System.out.println("Student details " + name +" "  + email+" " + phone +" "+ passout +" "+ backlogs);
+  
+  System.out.println("Student details are shown below " + '\n' + name + '\n' + email + '\n'  + phone + '\n' + passout + '\n' + backlogs);
+  
+	};
+
+}
