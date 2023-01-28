2a708931435cfd147e21024dd4a83cb9df2a2ce0
anupama.g <anupamagiris@gmail.com>
2023-01-28 22:06:22 +0530

    This is fourth file added to git repo

---
+++ IfDemo.java
@@ -0,0 +1,21 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//age is >45 old else young
+		int age=29;
+		//int age=50;
+		if (age>45)
+		{
+			System.out.println ("Person is old");
+
+		}
+	else
+	{
+		System.out.println ("Person is young");
+	}
+
+}
+
+}
